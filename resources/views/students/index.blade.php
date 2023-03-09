9b286aa252a38211fa503ead6d2d4639e0cea2e1
Dory Yang <yang8006yang@gmail.com>
2023-03-09 13:34:29 +0800

    前端先建一個表格

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,26 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <title>Students</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-        <h1>Hello, {{ $name }}</h1>
-        <a href="{{route('student.excel')}}">student_excel</a>
-        <a href="{{route('student.child')}}">student_child</a>
+<div class="container">
+  <h2>Basic Table</h2>
+  <p>The .table class adds basic styling (light padding and only horizontal dividers) to a table:</p>            
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
-        @foreach ($data as $user)
-        <p>{{ $user['name'] }}</p>
-        @endforeach
-        <h1>123</h1>
+{{-- <h1>Hello, {{ $name }}</h1>
+<a href="{{route('student.excel')}}">student_excel</a>
+<a href="{{route('student.child')}}">student_child</a>
 
-        @php
-        dd($data);
-        @endphp
+@foreach ($data as $user)
+<p>{{ $user['name'] }}</p>
+@endforeach
+<h1>123</h1>
 
-
+@php
+dd($data);
+@endphp --}}
 </body>
 </html>
